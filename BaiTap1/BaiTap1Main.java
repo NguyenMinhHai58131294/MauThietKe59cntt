20e717cc34b9a9a156d999019d32926b1597f88a
nguyen minh hai <haifafb12338@gmail.com>
2020-04-08 18:24:28 +0700

    MauThietKeBai1

--- BaiTap1/BaiTap1Main.java
+++ BaiTap1/BaiTap1Main.java
@@ -15,6 +15,20 @@ public class BaiTap1Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        NhanVien nv = new NhanVien();
+        nv.setTen("Nguyễn Minh Hải");
+        nv.setTuoi(22);
+        nv.setDiaChi("12 Lê Hông Phong");
+        nv.setTienLuong(2000);
+        nv.setTongSoGioLam(150);
+        NhanVien nv2 = new NhanVien();
+        nv2.setTen("Nguyễn Văn Tiến");
+        nv2.setTuoi(23);
+        nv2.setDiaChi("30 Lê Lợi");
+        nv2.setTienLuong(3000);
+        nv2.setTongSoGioLam(400);
+        System.out.println(""+nv.getThongTin() + " Tiền Thưởng " + nv.tinhThuong());
+        System.out.println(""+nv2.getThongTin()+" Tiền Thưởng "+nv.tinhThuong());
         // TODO code application logic here
     }
     
--- BaiTap1/NhanVien.java
+++ BaiTap1/NhanVien.java
@@ -10,5 +10,77 @@ package BaiTap1;
  * @author admin
  */
 public class NhanVien {
+    String ten;
+    int tuoi;
+    String diaChi;
+    double tienLuong;
+    int tongSoGioLam;
+
+    public NhanVien() {
+    }
+
+    public NhanVien(String ten, int tuoi, String diaChi, double tienLuong, int tongSoGioLam) {
+        this.ten = ten;
+        this.tuoi = tuoi;
+        this.diaChi = diaChi;
+        this.tienLuong = tienLuong;
+        this.tongSoGioLam = tongSoGioLam;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public double getTienLuong() {
+        return tienLuong;
+    }
+
+    public void setTienLuong(double tienLuong) {
+        this.tienLuong = tienLuong;
+    }
+
+    public int getTongSoGioLam() {
+        return tongSoGioLam;
+    }
+
+    public void setTongSoGioLam(int tongSoGioLam) {
+        this.tongSoGioLam = tongSoGioLam;
+    }
     
+    public String getThongTin()
+    {
+        return getTen()+" "+getTuoi()+" "+getDiaChi()+" "+getTienLuong();
+        
+    }
+    public double tinhThuong()
+    {
+        if(getTongSoGioLam()>=200)
+        {
+           return getTienLuong() * 20 / 100;
+        }else
+            if(getTongSoGioLam()>=100)
+            {
+                return getTienLuong() * 10 / 100;
+            }else return 0;
+    }
 }
